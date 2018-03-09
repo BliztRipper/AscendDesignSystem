81e5f856f44988064d9d990b44070d92d45f3dd8
Arnon Tawong <arnon.taw@AGC01435.local>
2018-03-09 15:52:41 +0700

    test side menu and transition

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,8 @@
 //@prepros-prepend ../../node_modules/jquery/dist/jquery.min.js
 //@prepros-prepend ../../node_modules/aos/dist/aos.js
 //@prepros-prepend ../../node_modules/bootstrap/dist/js/bootstrap.bundle.js
+
+
+$('img.menu-bars').click(function () {
+  $('.sidebar').toggleClass('active')
+})
