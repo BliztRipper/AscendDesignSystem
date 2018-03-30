fffe4594a3d9fb29c3dfeb602dcf7e7e70e67894
Arnon Tawong <arnon.taw@agc01278.ebusiness.th>
2018-03-30 09:39:42 +0700

    add parallax bg

--- src/script/main.js
+++ src/script/main.js
@@ -1,16 +1,4 @@
 //@prepros-prepend ../../node_modules/jquery/dist/jquery.min.js
 //@prepros-prepend ../../node_modules/aos/dist/aos.js
 //@prepros-prepend ../../node_modules/bootstrap/dist/js/bootstrap.bundle.js
-//@prepros-prepend ../../node_modules/jquery-lazy/jquery.lazy.min.js
 
-
-$('img.menu-bars').click(function () {
-  $('.sidebar').toggleClass('active')
-})
-
-$(function () {
-  $('.lazy-load').lazy({
-    enableThrottle: true,
-    throttle: 200
-  });
-}); 
