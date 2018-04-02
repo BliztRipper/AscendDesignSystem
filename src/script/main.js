8eb194ea226e091847df7f9bf15dc218f9a595d5
Arnon Tawong <arnon.taw@agc00641.ebusiness.th>
2018-04-02 15:39:06 +0700

    test function scroll

--- src/script/main.js
+++ src/script/main.js
@@ -5,11 +5,22 @@
 AOS.init();
 
 $(window).on("scroll", function () {
-  var scrollHeight = $(window).height();
-  var scrollPosition = $(window).scrollTop() + 60;
-  if (scrollPosition > scrollHeight) {
+  var scrollHeight = $(window).height(); //847
+  var navHeight = $('.navGrid').innerHeight(); //124
+  var sprintHeight = $('.main').innerHeight() + $('.sprint').innerHeight(); //1927
+  var scrollOne = $(window).scrollTop() + navHeight;
+  var scrollTwo = $(window).scrollTop() + sprintHeight + navHeight;
+  if (scrollOne > scrollHeight) {
     $('.navGrid').addClass('active');
+    $('.navGrid').find('a').removeClass('active');
+    $('.navGrid__menu-2' ).find( "a" ).addClass('active');
+  } else if(($(window).scrollTop() > 2800) ){
+    $('.navGrid').find('a').removeClass('active');
+    $('.navGrid__menu-3' ).find( "a" ).addClass('active');
   } else {
     $('.navGrid').removeClass('active');
+    $('.navGrid').find('a').removeClass('active');
+    $('.navGrid__menu-1' ).find( "a" ).addClass('active');
   }
 });
+
