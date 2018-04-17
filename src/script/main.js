074e3122176b39ea7b1c7f5f0ff0f970c9e14945
Arnon Tawong <arnon.taw@ARNON-MBP.local>
2018-04-17 15:30:59 +0700

    add animation

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,5 @@
 //@prepros-prepend ../../node_modules/jquery/dist/jquery.min.js
 //@prepros-prepend ../../node_modules/aos/dist/aos.js
 //@prepros-prepend ../../node_modules/bootstrap/dist/js/bootstrap.bundle.js
+
+AOS.init();
